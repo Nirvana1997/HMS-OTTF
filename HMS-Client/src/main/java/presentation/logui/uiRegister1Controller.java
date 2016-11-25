1ef90a6d7e0eea5d9388edbd1c23bcc27f8074ef
PC-20160912JZDS\Administrator <727929807@qq.com>
2016-11-25 19:54:48 +0800

    界面：注册123步骤的图片和跳转

--- HMS-Client/src/main/java/presentation/logui/uiRegister1Controller.java
+++ HMS-Client/src/main/java/presentation/logui/uiRegister1Controller.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -17,9 +18,30 @@ public class uiRegister1Controller {
      * 从注册界面1返回到登录界面
      */
     public Button buttonReturn;
+    ObservableList<Stage> stage = FXRobotHelper.getStages();
     public void returnToLog() throws IOException {
-        ObservableList<Stage> stage = FXRobotHelper.getStages();
+
         Scene scene = new Scene((Parent) FXMLLoader.load(getClass().getResource("sceneLog.fxml")));
         stage.get(0).setScene(scene);
+    }
+
+    /**
+     * 根据账号密码选择报错或者进行下一步
+     */
+
+    public Button buttonNext;
+    public TextField textUserID;
+    public TextField textPassword;
+    public TextField textConfirm;
+
+    public void goToRegister2() throws IOException {
+        String UserID = textUserID.getText();
+        String Password = textPassword.getText();
+        String Confirm = textConfirm.getText();
+        //TODO 判断条件需要修改
+//        ObservableList<Stage> stage = FXRobotHelper.getStages();
+        Scene scene = new Scene((Parent) FXMLLoader.load(getClass().getResource("sceneRegister2.fxml")));
+        stage.get(0).setScene(scene);
+
     }
 }
