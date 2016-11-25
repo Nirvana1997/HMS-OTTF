a9a1f591c82a0f97fba60a47e0c70b8cc5cde2f7
PC-20160912JZDS\Administrator <727929807@qq.com>
2016-11-25 14:13:19 +0800

    界面：登录跳到注册

--- HMS-Client/src/main/java/uiLog/uiLogController.java
+++ HMS-Client/src/main/java/uiLog/uiLogController.java
@@ -1,9 +1,17 @@
 package uiLog;
 
+import com.sun.javafx.robot.impl.FXRobotHelper;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by thinkpad on 2016/11/16.
@@ -21,5 +29,11 @@ public class uiLogController {
     public void setNextPic(){
         imgHotel01.setVisible(false);
         imgHotel02.setVisible(true);
+    }
+    public Button buttonRegister;
+    public void gotoRegister1() throws IOException {
+        ObservableList<Stage> stage = FXRobotHelper.getStages();
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("sceneRegister1.fxml")));
+        stage.get(0).setScene(scene);
     }
 }
---
+++ HMS-Client/src/main/java/uiLog/uiRegister1Controller.java
@@ -0,0 +1,7 @@
+package uiLog;
+
+/**
+ * Created by Administrator on 2016/11/20.
+ */
+public class uiRegister1Controller {
+}
