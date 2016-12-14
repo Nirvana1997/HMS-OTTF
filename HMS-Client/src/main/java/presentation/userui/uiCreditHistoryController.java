e10363cef6964abbf959b74c1c6505592b638057
PC-20160912JZDS\Administrator <727929807@qq.com>
2016-12-14 18:23:43 +0800

    - -

---
+++ HMS-Client/src/main/java/presentation/userui/uiCreditHistoryController.java
@@ -0,0 +1,112 @@
+package presentation.userui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Administrator on 2016/12/14.
+ */
+public class uiCreditHistoryController implements Initializable{
+    @FXML
+    /**
+     * 跳转到首页的按钮
+     */
+    private Button buttonHomePage;
+    @FXML
+    /**
+     * 跳转到我的订单的按钮
+     */
+    private Button buttonMyOrder;
+    @FXML
+    /**
+     * 跳转到浏览酒店的按钮
+     */
+    private Button buttonReadHotel;
+    @FXML
+    /**
+     * 跳转到搜索酒店的按钮
+     */
+    private Button buttonSearchHotel;
+    @FXML
+    /**
+     * 跳转到个人信息的按钮
+     */
+    private Button buttonUserInfo;
+    @FXML
+    /**
+     * 登出按钮
+     */
+    private Label buttonLogOut;
+
+    /**
+     * 搜索按钮
+     */
+    @FXML
+    private Button buttonSearch;
+
+    /**
+     * 跳转界面的类
+     */
+    sceneJump jump = new sceneJump();
+
+    /**
+     * 跳转到首页
+     * @throws IOException
+     */
+    public void gotoHomePage() throws IOException {
+        jump.gotoHomePage();
+    }
+
+    /**
+     * 跳转到我的订单
+     * @throws IOException
+     */
+    public void gotoMyOrder() throws IOException{
+        jump.gotoMyOrder();
+    }
+
+    /**
+     * 跳转到浏览酒店
+     * @throws IOException
+     */
+    public void gotoReadHotel() throws IOException{
+        jump.gotoReadHotel();
+    }
+
+    /**
+     * 跳转到搜索酒店
+     * @throws IOException
+     */
+    public void gotoSearchHotel() throws IOException{
+        jump.gotoSearchHotel();
+    }
+
+    /**
+     * 跳转到个人信息
+     * @throws IOException
+     */
+    public void gotoUserInfo() throws IOException{
+        jump.gotoUserInfo();
+    }
+
+    /**
+     * 清空账号信息，并跳转回登录界面
+     * @throws IOException
+     */
+    public void LogOut() throws IOException{
+        //TODO 清空账号
+        jump.gotoLogin();
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //TODO 初始化信用变化记录
+    }
+}
