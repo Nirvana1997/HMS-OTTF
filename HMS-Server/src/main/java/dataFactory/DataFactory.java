44080ceeabce2eb91dd7c812f7707bc351bbe8e6
Snow-96 <511422713@qq.com>
2016-12-19 13:37:40 +0800

    Sever增加工厂模式包

---
+++ HMS-Server/src/main/java/dataFactory/DataFactory.java
@@ -0,0 +1,22 @@
+package dataFactory;
+import dataservice.hotelsalerdataservice.HotelinfoDataService;
+import dataservice.hotelsalerdataservice.HotelroomDataService;
+import dataservice.logdataservice.LogDataService;
+import dataservice.orderdataservice.OrderDataService;
+import dataservice.promotiondataservice.PromotionDataService;
+import dataservice.userdataservice.UserDataService;
+import dataservice.websalerdataservice.WebsalerDataService;
+import java.rmi.Remote;
+
+/**
+ * Created by mac on 2016/12/19.
+ */
+public interface DataFactory extends Remote{
+    public HotelinfoDataService getHotelinfoDataImpl()throws Exception;
+    public HotelroomDataService getHotelroomDataImpl()throws Exception;
+    public LogDataService getLogDataImpl()throws Exception;
+    public OrderDataService getOrderDataImpl()throws Exception;
+    public PromotionDataService getPromotionDataImpl()throws Exception;
+    public UserDataService getUserDataImpl()throws Exception;
+    public WebsalerDataService getWebsalerDataImpl()throws Exception;
+}
---
+++ HMS-Server/src/main/java/dataFactory/DataFactoryImpl.java
@@ -0,0 +1,62 @@
+package dataFactory;
+
+import data.hotelsalerdata.HotelinfoDataImpl;
+import data.hotelsalerdata.HotelroomDataImpl;
+import data.logdata.LogDataImpl;
+import data.orderdata.OrderDataImpl;
+import data.promotiondata.PromotionDataImpl;
+import data.userdata.UserDataImpl;
+import data.websalerdata.WebsalerDataImpl;
+import dataservice.hotelsalerdataservice.HotelinfoDataService;
+import dataservice.hotelsalerdataservice.HotelroomDataService;
+import dataservice.logdataservice.LogDataService;
+import dataservice.orderdataservice.OrderDataService;
+import dataservice.promotiondataservice.PromotionDataService;
+import dataservice.userdataservice.UserDataService;
+import dataservice.websalerdataservice.WebsalerDataService;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ * Created by mac on 2016/12/19.
+ */
+public class DataFactoryImpl extends UnicastRemoteObject implements DataFactory{
+    public DataFactoryImpl() throws RemoteException {
+    }
+
+    @Override
+    public HotelinfoDataService getHotelinfoDataImpl() throws Exception {
+        return new HotelinfoDataImpl();
+    }
+
+    @Override
+    public HotelroomDataService getHotelroomDataImpl() throws RemoteException {
+        return new HotelroomDataImpl();
+    }
+
+    @Override
+    public LogDataService getLogDataImpl() throws Exception {
+        return new LogDataImpl();
+    }
+
+    @Override
+    public OrderDataService getOrderDataImpl() throws RemoteException {
+        return new OrderDataImpl();
+    }
+
+    @Override
+    public PromotionDataService getPromotionDataImpl() throws RemoteException {
+        return new PromotionDataImpl();
+    }
+
+    @Override
+    public UserDataService getUserDataImpl() throws Exception {
+        return new UserDataImpl();
+    }
+
+    @Override
+    public WebsalerDataService getWebsalerDataImpl() throws Exception {
+        return new WebsalerDataImpl();
+    }
+}
